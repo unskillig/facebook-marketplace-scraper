ee94eeda036af971a470e5180e9acae44ac0262a
nils <nils.hackstein2@gmail.com>
2020-03-29 20:20:36 +0200

    extended codebase

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const getItems = async searchTerm => {
     const page = await browser.newPage();
 
     await page.goto(`https://facebook.com/marketplace/search/?query=${encodeURI(searchTerm)}`);
+
+    await page.waitForSelector('div > div > span > div > a[tabindex="0"]')
+    .then(() => page.evaluate(() => {
+        const itemArray = [];
+        const itemNodeList = document.querySelectorAll('div > div > span > div > a[tabindex="0"]');
+        
+        itemNodeList.forEach(item => {
+            const itemTitle = item.getElementsByClassName('l9j0dhe7 stjgntxs ni8dbmo4').value;
+            console.log(itemTitle);
+        });
+
+    }))
+    .catch(() => console.log("Selector error."));
+
 }
 
-getItems('Iphone 8');
+getItems('Iphone X');
+
+
